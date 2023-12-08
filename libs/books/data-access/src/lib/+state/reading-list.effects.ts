16ec0ff320a312476aafd5466ac6dd57e0719b91
Sherin Roy <Stuart.Hopkins22@T-Mobile.com>
2023-12-08 18:42:50 +0530

    undo actions code changes

--- libs/books/data-access/src/lib/+state/reading-list.effects.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType, OnInitEffects } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { catchError, concatMap, exhaustMap, map } from 'rxjs/operators';
-import { ReadingListItem } from '@tmo/shared/models';
+import { catchError, concatMap, exhaustMap, filter, map } from 'rxjs/operators';
+import { Book, ReadingListItem } from '@tmo/shared/models';
 import * as ReadingListActions from './reading-list.actions';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Store } from '@ngrx/store';
+import { ReadingListConstants } from '../reading-list.contants';
 
 @Injectable()
 export class ReadingListEffects implements OnInitEffects {
@@ -54,9 +57,70 @@ export class ReadingListEffects implements OnInitEffects {
     )
   );
 
+  undoAddtoReadingList$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(ReadingListActions.confirmedAddToReadingList),
+        filter(({ book }) => book.isOpenSnackBar),
+        map(({ book }) =>
+          this.openSnackBar(
+            { ...book, bookId: book.id, isOpenSnackBar: false },
+            `${book.title}: ${ReadingListConstants.ACTION_ADD_MSG}`,
+            true
+          )
+        )
+      ),
+    { dispatch: false }
+  );
+  undoRemoveFromReadingList$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(ReadingListActions.confirmedRemoveFromReadingList),
+        filter(({ item }) => item.isOpenSnackBar),
+        map(({ item }) =>
+          this.openSnackBar(
+            { ...item, id: item.bookId, isOpenSnackBar: false },
+            `${item.title}: ${ReadingListConstants.ACTION_REMOVE_MSG}`,
+            false
+          )
+        )
+      ),
+    { dispatch: false }
+  );
+
+  openSnackBar(
+    item: ReadingListItem | Book,
+    message: string,
+    isAdded: boolean
+  ): void {
+    this.snackBar
+      .open(message, ReadingListConstants.UNDO, {
+        duration: ReadingListConstants.DURATION,
+      })
+      .onAction()
+      .subscribe(() =>
+        isAdded
+          ? this.store.dispatch(
+              ReadingListActions.removeFromReadingList({
+                item: item as ReadingListItem,
+              })
+            )
+          : this.store.dispatch(
+              ReadingListActions.addToReadingList({
+                book: item as Book,
+              })
+            )
+      );
+  }
+
   ngrxOnInitEffects() {
     return ReadingListActions.init();
   }
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    private store: Store
+  ) {}
 }
