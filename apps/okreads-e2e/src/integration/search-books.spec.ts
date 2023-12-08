0ceb1b007099c33e9f9cc254121183cb998b8365
Sherin Roy <Stuart.Hopkins22@T-Mobile.com>
2023-12-08 15:06:59 +0530

    instant search code changes

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -3,15 +3,9 @@ describe('When: Use the search feature', () => {
     cy.startAt('/');
   });
 
-  it('Then: I should be able to search books by title', () => {
+  it('Then: I should be able to see search results as I am typing', () => {
     cy.get('input[type="search"]').type('javascript');
 
-    cy.get('form').submit();
-
     cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
-  });
-
-  xit('Then: I should see search results as I am typing', () => {
-    // TODO: Implement this test!
   });
 });
--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,27 +1,98 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { searchBooks } from '@tmo/books/data-access';
 
-describe('ProductsListComponent', () => {
+describe('BookSearchComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
+  let store: MockStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule],
+      providers: [
+        provideMockStore({
+          initialState: { books: { entities: [] } },
+        }),
+      ],
     }).compileComponents();
+    store = TestBed.inject(MockStore);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    jest.spyOn(store, 'dispatch');
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
+  });
+
+  describe('searchBooks()', () => {
+    it('should see the search results for all distinct input in every 500ms', fakeAsync(() => {
+      component.searchForm.controls.term.setValue('Javascript');
+      tick(500);
+
+      expect(store.dispatch).toHaveBeenCalledWith(
+        searchBooks({ term: 'Javascript' })
+      );
+
+      component.searchForm.controls.term.setValue('Java');
+      tick(500);
+
+      expect(store.dispatch).toHaveBeenCalledWith(
+        searchBooks({ term: 'Java' })
+      );
+    }));
+
+    it('should not see the search results when input received before 500ms', fakeAsync(() => {
+      component.searchForm.controls.term.setValue('Javascript');
+      tick(500);
+
+      component.searchForm.controls.term.setValue('Java');
+      tick(300);
+
+      component.searchForm.controls.term.setValue('Javascript');
+      tick(500);
+
+      expect(store.dispatch).toHaveBeenCalledWith(
+        searchBooks({ term: 'Javascript' })
+      );
+    }));
+
+    it('should see the existing search result when received same input for search term', fakeAsync(() => {
+      component.searchForm.controls.term.setValue('Javascript');
+      tick(500);
+
+      component.searchForm.controls.term.setValue('Javascript');
+      tick(500);
+
+      expect(store.dispatch).toHaveBeenCalledTimes(1);
+    }));
+  });
+
+  describe('ngOnDestroy()', () => {
+    it('should unsubscribe to input stream when component is destroyed', fakeAsync(() => {
+      component.ngOnDestroy();
+
+      component.searchForm.controls.term.setValue('Javascript');
+
+      tick(500);
+
+      expect(store.dispatch).not.toHaveBeenCalled();
+    }));
   });
 });
